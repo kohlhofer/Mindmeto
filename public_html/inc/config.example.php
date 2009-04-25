46f3bdc9c023eeaaec0a6d1de692db7f9e8e1962
Alexander Kohlhofer <alexander@kohlhofer.com>
2009-04-25 16:39:54 +0200

    removed sensible data

---
+++ public_html/inc/config.example.php
@@ -0,0 +1,59 @@
+<?php
+
+//===========================================================================
+// General settings
+//===========================================================================
+
+define('BASE_URL', 'http://nial.me/experiments/mindmeto/');
+define('REMINDER_PERIOD', 1);
+
+//===========================================================================
+// Email settings
+//===========================================================================
+
+define('MAIL_SERVER', 'mail.mindmeto.com');
+define('MAIL_USERNAME', 'bot@mindmeto.com');
+define('MAIL_PASSWORD', '');
+
+//===========================================================================
+// Twitter settings
+//===========================================================================
+
+define('TWITTER_ID', 29181978);
+define('TWITTER_DM_CONFIRMATION', 0);
+define('TWITTER_DM_REMINDER', 1);
+
+//===========================================================================
+// OAuth settings
+//===========================================================================
+
+define('OAUTH_CONSUMER_KEY', '');
+define('OAUTH_CONSUMER_SECRET', '');
+define('OAUTH_TOKEN', '');
+define('OAUTH_TOKEN_SECRET', '');
+
+//===========================================================================
+// Database settings
+//===========================================================================
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'nial_mindmeto');
+define('DB_USERNAME', 'nial_mmt');
+define('DB_PASSWORD', '');
+
+//===========================================================================
+// Database table settings
+//===========================================================================
+
+define('DB_TBL_USERS', 'users');
+define('DB_TBL_OPTIONS', 'options');
+define('DB_TBL_REMINDERS', 'reminders');
+
+//===========================================================================
+// Cookie settings
+//===========================================================================
+
+define('COOKIE_PATH', '/');
+define('COOKIE_EXPIRE', 157784630);
+
+?>
